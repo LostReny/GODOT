f1c492c0fdcc028695d76c66e3cc048c3197e48f
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2025-06-03 12:15:11 -0300

    player movement

--- ProjectBoost/Project Boost/player.gd
+++ ProjectBoost/Project Boost/player.gd
@@ -1,10 +1,12 @@
 extends Node3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_physical_key_pressed(KEY_SPACE):
+		position.y += delta
+	
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+	elif Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
 	pass
