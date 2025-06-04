09270dac92fbae154452c842bcce7ec7b62c70a3
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2025-06-04 10:49:28 -0300

    player input and movement

--- ProjectBoost/Project Boost/player.gd
+++ ProjectBoost/Project Boost/player.gd
@@ -1,12 +1,18 @@
 extends RigidBody3D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@export var speed : float = 1000.0
+@export var torque_z_axis : float = 100.0
+
 func _process(delta: float) -> void:
-	if Input.is_physical_key_pressed(KEY_SPACE):
-		apply_central_force(Vector3.UP)
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * speed)
 	
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
-	elif Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0.0,0.0, torque_z_axis * delta))
+	elif Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0,0.0,-torque_z_axis * delta))
 	pass
+
+
+func _on_body_entered(body: Node) -> void:
+	pass # Replace with function body.
