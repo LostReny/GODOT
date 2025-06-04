9146598db465e3ea4626f83127e565fd122602fd
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2025-06-04 12:24:20 -0300

    upgrade

--- ProjectBoost/Project Boost/player.gd
+++ ProjectBoost/Project Boost/player.gd
@@ -1,18 +1,30 @@
 extends RigidBody3D
 
-@export var speed : float = 1000.0
-@export var torque_z_axis : float = 100.0
+## How much vertical force apply when moving
+@export_range(750.0,3000.0) var force : float = 1000.0
+
+## How much rotational force apply on Z axis
+@export var torque_force : float = 100.0
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * speed)
+		apply_central_force(basis.y * delta * force)
 	
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0,0.0, torque_z_axis * delta))
+		apply_torque(Vector3(0.0,0.0, torque_force * delta))
 	elif Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0,0.0,-torque_z_axis * delta))
+		apply_torque(Vector3(0.0,0.0,-torque_force * delta))
 	pass
 
 
 func _on_body_entered(body: Node) -> void:
-	pass # Replace with function body.
+	if "Goal" in body.get_groups():
+		print("Win!")
+	elif "Ground" in body.get_groups():
+		crash_sequence()
+	pass
+	
+	
+func crash_sequence() -> void:
+	print("kabbom!")
+	pass
