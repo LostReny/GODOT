c606c0e84f2b37d00526639a9ebad319f2af7f8e
Isabelle Reny <isabelleribaldo@hotmail.com>
2025-06-18 22:48:13 -0300

    VFX
    Particles booster, explosion and level complete

--- ProjectBoost/Project Boost/player.gd
+++ ProjectBoost/Project Boost/player.gd
@@ -7,24 +7,41 @@ extends RigidBody3D
 @export var torque_force : float = 100.0
 
 #variable that will be called when _ready function start
+#Sound
 @onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
 @onready var sucess_audio: AudioStreamPlayer = $SucessAudio
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+#VFX
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles : GPUParticles3D = $RightBoosterParticles
+@onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
+@onready var success_particles: GPUParticles3D = $SuccessParticles
+
+# mesh
+#@onready var mesh_instance_3d = $MeshInstance3D
 
 var is_transitioning: bool = false
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * force)
+		booster_particles.emitting = true
 		if rocket_audio.playing == false:
 			rocket_audio.play()
 	else:
+		booster_particles.emitting = false
 		rocket_audio.stop()
 		
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0,0.0, torque_force * delta))
+		right_booster_particles.emitting = true
 	elif Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0,0.0,-torque_force * delta))
+		left_booster_particles.emitting = true
+	else:
+		right_booster_particles.emitting = false
+		left_booster_particles.emitting = false
 	pass
 
 
@@ -39,6 +56,11 @@ func _on_body_entered(body: Node) -> void:
 # when the player collides with the ground
 func crash_sequence() -> void:
 	print("kabbom!")
+	explosion_particles.emitting = true
+	#mesh_instance_3d.visible = false
+	booster_particles.emitting = false
+	right_booster_particles.emitting = false
+	left_booster_particles.emitting = false
 	rocket_audio.stop()
 	explosion_audio.play()
 	set_process(false)
@@ -51,6 +73,10 @@ func crash_sequence() -> void:
 # function when the player collides with the landing pad
 func complete_level(next_level_file: String) -> void:
 	print("Level Complete!")
+	success_particles.emitting = true
+	booster_particles.emitting = false
+	right_booster_particles.emitting = false
+	left_booster_particles.emitting = false
 	rocket_audio.stop()
 	sucess_audio.play()
 	set_process(false)
