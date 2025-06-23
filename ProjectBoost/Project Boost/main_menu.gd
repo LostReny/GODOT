37cdc99d37c012ebb7342cc337ee510e89f2a28f
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2025-06-23 18:09:57 -0300

    Tela de como jogar

--- ProjectBoost/Project Boost/main_menu.gd
+++ ProjectBoost/Project Boost/main_menu.gd
@@ -1,13 +1,14 @@
 extends Control
 
 @export_file("*.tscn") var start_path : String
+@export_file("*.tscn") var intructions_path : String
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file(start_path)
 
 
 func _on_htp_button_pressed():
-	# painel com intruções de como jogar
+	get_tree().change_scene_to_file(intructions_path)
 	pass # Replace with function body.
 
 
