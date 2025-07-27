cbaec24472cf171fc121696727a0386c6092310a
Isabelle Reny <isabelleribaldo@hotmail.com>
2025-07-27 19:47:01 -0300

    upgrade list

--- GameIdeaCheatSheet/CityBuilder/clickTarget.gd
+++ GameIdeaCheatSheet/CityBuilder/clickTarget.gd
@@ -3,7 +3,12 @@ extends Area2D
 var click_counter : int = 0
 
 ## value to reach the upgrade point
-@export var upgrade_value : int
+@export var upgrade_value : Array[int] = []
+
+##sprites
+@onready var sprite_2d = $"../Sprite2D"
+@onready var sprite_upgrade = $"../Sprite_upgrade"
+
 
 #função de ler quando clica
 func _input_event(viewport, event, shape_idx):
@@ -20,6 +25,11 @@ func _click_counter() -> void:
 
 #upgrade funcition
 func _upgrade() -> void:
-	if click_counter == upgrade_value:
-		print("upgrade")
+	if click_counter == upgrade_value[0]:
+		print("upgrade1")
+		sprite_upgrade.visible = true
+		sprite_2d.visible = false
+	elif click_counter == upgrade_value[1]:
+		print("upgrade2")
+		
 	pass
