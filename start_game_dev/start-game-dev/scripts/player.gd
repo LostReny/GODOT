04211537806dc67ab8499b049ac38e387c17e9fa
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2026-01-19 11:45:45 -0300

    sprite_move

--- start_game_dev/start-game-dev/scripts/player.gd
+++ start_game_dev/start-game-dev/scripts/player.gd
@@ -2,27 +2,36 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
-var input_direction : get = _get_input_direction
-var sprite_direction
-var is_moving = false
+var input_direction : Vector2 = Vector2.ZERO
+var is_moving : bool = false
 
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
+	_get_input_direction()
 	velocity = input_direction * SPEED
-	is_moving == true
 	move_and_slide()
+	_get_sprite()
 
 
 func _get_input_direction():
 	var x = -int(Input.is_action_pressed("LEFT")) + int(Input.is_action_pressed("RIGHT"))
 	var y = -int(Input.is_action_pressed("UP")) + int(Input.is_action_pressed("DOWN"))
+	
 	input_direction = Vector2(x,y).normalized()
-	return input_direction
+	is_moving = input_direction != Vector2.ZERO
+	#return input_direction
 
-#fazer sprite de correr quando se mover 
-#fazer sprite mudar de lado, quando for pra esquerda
+
 func _get_sprite():
-	if  is_moving == true:
-		sprite = "run"
-		pass
+	if is_moving:
+		if sprite.animation != "run":
+			sprite.play("run")
+	else:
+		if sprite.animation != "idle":
+			sprite.play("idle")
+	pass
+
+#fazer sprite mudar de lado, quando for pra esquerda
+func _flip_sprite():
+	pass
